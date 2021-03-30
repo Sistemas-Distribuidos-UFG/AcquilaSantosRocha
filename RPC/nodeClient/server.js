9ab70972e4e0cea3bc39905b92752014ad714e19
Acquila Santos Rocha <acquila.santos@gmail.com>
2021-03-29 23:06:40 -0300

    RPC connection implemented, but error while getting response from rpc queue

--- RPC/nodeClient/server.js
+++ RPC/nodeClient/server.js
@@ -1,12 +1,17 @@
 const amqp =  require('amqplib');
 const express = require('express');
+const bodyParser = require('body-parser');
 const questions = require("./question.json");
+const uuid = require('node-uuid');
 
 
 const rabbitMqUri = 'amqp://localhost';
 const port = 8000;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + "/public"));
@@ -29,30 +34,37 @@ app.get('/question', (req, res) => {
     });
 });
 
+
+app.post('/request', (req, res) => {
+    console.log('Got body:', req.body);
+    var input = req.body;
+    // console.log(res);
+    makeRpcRequest(input).then(result => {
+        console.log(result);
+        result = JSON.parse(result);
+        console.log(result);
+        // const filename = result.filename;
+    });
+    res.sendStatus(200);
+});
+
+
 function makeRpcRequest(channel, data) {
 
-    const deferred = Promise.defer();
     const correlationId = uuid();
-
-    function maybeAnswer(message) {
-        if (message.properties.correlationId === correlationId) {
-            deferred.resolve(message.content.toString());
-        }
-    }
-
+    console.log(data);
     return channel.assertQueue('', { exclusive: true }).then(result => {
         const queue = result.queue;
 
-        return channel.consume(queue, maybeAnswer, { noAck: true }).then(() => {
+        return channel.consume(queue, '', { noAck: true }).then(() => {
 
             console.log('Performing RPC request: ' + correlationId);
-
-            channel.sendToQueue('rpc_queue', new Buffer(data), {
+            console.log(JSON.stringify(data));
+            channel.sendToQueue('rpc_queue', Buffer.from(JSON.stringify(data)), {
                 correlationId: correlationId,
                 replyTo: queue
             });
 
-            return deferred.promise;
         });
     });
 }
@@ -62,7 +74,6 @@ function handleError(error) {
     console.error(error.stack);
     console.log('Continuing …');
 }
-
 
 function startRabbitMq(port)
 {
