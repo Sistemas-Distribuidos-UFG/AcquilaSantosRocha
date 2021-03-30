bf38ca52f41319a69cc5f7d329f6d867a9f40aba
Acquila Santos Rocha <acquila.santos@gmail.com>
2021-03-29 21:33:35 -0300

    Adding input to questions and configuring basic for rabbitMQ handlers in nodeJS

--- RPC/nodeClient/server.js
+++ RPC/nodeClient/server.js
@@ -1,10 +1,14 @@
+const amqp =  require('amqplib');
 const express = require('express');
 const questions = require("./question.json");
+
+
+const rabbitMqUri = 'amqp://localhost';
+const port = 8000;
 
 const app = express();
 
 app.set('view engine', 'pug');
-
 app.use(express.static(__dirname + "/public"));
 
 
@@ -25,9 +29,65 @@ app.get('/question', (req, res) => {
     });
 });
 
-// app.engine('pug', require('pug').__express)
+function makeRpcRequest(channel, data) {
+
+    const deferred = Promise.defer();
+    const correlationId = uuid();
+
+    function maybeAnswer(message) {
+        if (message.properties.correlationId === correlationId) {
+            deferred.resolve(message.content.toString());
+        }
+    }
+
+    return channel.assertQueue('', { exclusive: true }).then(result => {
+        const queue = result.queue;
+
+        return channel.consume(queue, maybeAnswer, { noAck: true }).then(() => {
+
+            console.log('Performing RPC request: ' + correlationId);
+
+            channel.sendToQueue('rpc_queue', new Buffer(data), {
+                correlationId: correlationId,
+                replyTo: queue
+            });
+
+            return deferred.promise;
+        });
+    });
+}
+
+function handleError(error) {
+    // FIXME: Handle properly
+    console.error(error.stack);
+    console.log('Continuing …');
+}
 
 
+function startRabbitMq(port)
+{
+    amqp.connect(rabbitMqUri).then(connection => {
+
+        return connection.createChannel().then(channel => {
+
+            makeRpcRequest = makeRpcRequest.bind(null, channel);
+
+            // (Would use HTTPS for production)
+            const server = app.listen(port, () => {
+                console.log('Amqp server running → PORT ' + port);
+            });
+
+            return server;
+        })
+        .catch(handleError);
+    })
+    .catch(error => {
+        console.error('Could not connect to RabbitMQ server\n\n' + error.stack);
+        process.exit(1);
+    });
+}
+
+startRabbitMq(port);
 
 const server = app.listen(7000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
